3310aeac92860c2e8ed10ef9958e876c7ae34c61
¨Breyner <lfonsobreyner@gmail.com>
2025-12-10 17:29:09 -0500

    cambios oara railway

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "tramway.proxy.rlwy.net",
+  host: "centerbeam.proxy.rlwy.net",
   user: "root",
-  password: "mgnqyZmnUZcjMeOIigXpEuvGmtijiNiz",
+  password: "XiMjGOrKBPoKGfnLBluHdwxdyBFUJRbG",
+  port: 59739,
   database: "railway",
-  port: 26013
 });
 
 export default db;
